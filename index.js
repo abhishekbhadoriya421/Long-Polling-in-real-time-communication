cf9023e692a7e7fce70647d0cf446f40915014a8
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-31 21:39:34 +0530

    created simple polling system

--- index.js
+++ index.js
@@ -3,7 +3,31 @@ const app = express();
 const port = 8080;
 const path = require('path');
 
+let clients = [];
 
+app.get('/poll', (req, res) => {
+    console.log("Connecttion Stablished with client... ");
+
+    clients.push(res);
+
+    req.on("close", () => {
+        clients = clients.filter(client => client !== res);
+    });
+});
+
+setInterval(() => {
+    if (clients.length >= 1) {
+        console.log("Broadcasting new data...");
+        clients.forEach(res => res.json({ message: "Hello at " + new Date() }));
+        clients = [];
+    }
+
+}, 3000);
+
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname + '/public/index.html');
+    return res.sendFile(filePath)
+})
 
 app.listen(port, (err) => {
     console.log('Server is running');
