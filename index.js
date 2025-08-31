4641182af6927adc09ca5a454c8a78c3e4bb0c6a
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-31 20:46:34 +0530

    initializing app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const path = require('path');
+
+
+
+app.listen(port, (err) => {
+    console.log('Server is running');
+})
+
+
